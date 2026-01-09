9a8654658fe364514ecefa1fb4841df7bbd0ec9a
rodrigoramooos <a2023137922@alumni.iscac.pt>
2026-01-09 17:01:56 +0000

    Época Normal

--- includes/footer.php
+++
@@ -1,8 +0,0 @@
-  <!-- Bootstrap 5 JS -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-  
-  <?php if (isset($additionalScripts)): ?>
-  <?php echo $additionalScripts; ?>
-  <?php endif; ?>
-</body>
-</html>
